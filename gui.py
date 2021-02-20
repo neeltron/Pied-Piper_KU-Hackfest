3c2c9f7c2805d5d90f6876bdf58fa09595d65d54
zigbee.sh@gmail.com <zigbee.sh@gmail.com>
2021-02-20 16:52:20 +0530

    Added dark theme

--- gui.py
+++ gui.py
@@ -4,11 +4,11 @@ import tkinter.font as tkFont
 from PIL import ImageTk, Image
 
 root = Tk()
-
+root["bg"]="black"
 
 fontStyle = tkFont.Font(family="Lucida Grande", size=15)
 
-label0 = Label(root,text="Enter parameters: ",font=fontStyle,anchor=W)
+label0 = Label(root,text="Enter parameters: ",font=fontStyle,anchor=W,fg="white",bg="black")
 label0.grid(row=0,column=0,columnspan=5,sticky=W+E)
 
 """
@@ -79,17 +79,23 @@ e12 = Entry(root,bd=2)
 label=[label1,label2,label3,label4,label5,label6,label7,label8,label9,label10,label11,label12]
 for index,lbl in enumerate(label):
     lbl.grid(row=index+1,column=0,pady=10)
+    lbl["bg"]="black"
+    lbl["fg"]="white"
 
 lst=[e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12]
 for index,e in enumerate(lst):
     e.grid(row=index+1,column=1,columnspan=3,padx=10,pady=10)
+    e["bg"]="black"
+    e["fg"]="white"
+    e["insertbackground"]="white"
 
 # Function to open another window 
 def open():
     top=Toplevel()
+    top["bg"]="black"
     top.title("analysis")
-    Label(top,text="Done").grid(row=0,column=0)
-    Button(top,text="Exit",command=top.destroy).grid(row=1,column=0)
+    Label(top,text="Done",bg="black",fg="white").grid(row=0,column=0)
+    Button(top,text="Exit",bg="black",fg="white",command=top.destroy).grid(row=1,column=0)
 
 # Functionn to clear the existing values
 def clear():
@@ -98,8 +104,8 @@ def clear():
         e.delete(0,END)
 
 #Buttons
-Button(root,text="Submit",command=open).grid(row=13,column=0,columnspan=5,sticky=W+E,pady=5)
-Button(root,text="CLear All",command=clear).grid(row=14,column=0,columnspan=5,sticky=W+E)
+Button(root,text="Submit",command=open,bg="black",fg="white").grid(row=13,column=0,columnspan=5,sticky=W+E,pady=5)
+Button(root,text="CLear All",command=clear,bg="black",fg="white").grid(row=14,column=0,columnspan=5,sticky=W+E)
 
 
 root.mainloop()
